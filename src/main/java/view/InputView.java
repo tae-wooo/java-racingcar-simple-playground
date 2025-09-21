0741dfcdde3da79891ad5b66aa04a78cc4ba6a11
taewoo <dnxorla03@naver.com>
2025-09-22 02:20:35 +0900

    InputView 입력 및 예외 처리 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -9,12 +10,19 @@ public class InputView {
 
     public static List<String> inputCarName() {
         System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
-        String inputName = scanner.nextLine();
+        String inputName = scanner.nextLine().replaceAll(" ", "");
         return Arrays.asList(inputName.split(","));
     }
 
     public static int inputRound() {
         System.out.println("시도할 회수는 몇회인가요?");
-        return scanner.nextInt();
+        while (true) {
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException input) {
+                System.out.println("회수는 숫자로 입력해 주세요!!");
+                scanner.nextLine();
+            }
+        }
     }
 }
